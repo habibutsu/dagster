d5c96235145eb372ea385ffa106c3bbcaf69f570
Nate Kupp <nate@elementl.com>
2020-02-05 10:09:45 -0800

    Pulling in a few EMR updates
    Summary:
    
    Test Plan: unit
    
    Reviewers:
    
    Subscribers:

--- python_modules/libraries/dagster-aws/dagster_aws/emr/emr.py
+++ python_modules/libraries/dagster-aws/dagster_aws/emr/emr.py
@@ -39,6 +39,10 @@ _FALLBACK_SERVICE_ROLE = 'EMR_DefaultRole'
 _FALLBACK_INSTANCE_PROFILE = 'EMR_EC2_DefaultRole'
 
 
+class EmrError(Exception):
+    pass
+
+
 class EmrJobRunner:
     def __init__(
         self, region, check_cluster_every=30, aws_access_key_id=None, aws_secret_access_key=None,
@@ -63,9 +67,41 @@ class EmrJobRunner:
         )
         return _wrap_aws_client(raw_emr_client, min_backoff=self.check_cluster_every)
 
+    def cluster_id_from_name(self, cluster_name):
+        '''Get a cluster ID in the format j-123ABC123ABC1 given a cluster name.
+
+        Args:
+            cluster_name (str): The name of the cluster for which to find an ID
+
+        Returns:
+            str: The ID of the cluster
+
+        Raises:
+            EmrError: No cluster with the specified name exists
+        '''
+        response = self.make_emr_client().list_clusters()
+        if response['Clusters']:
+            for cluster in response['Clusters']:
+                if cluster['Name'] == cluster_name:
+                    return cluster['Id']
+
+        raise EmrError(
+            'cluster {cluster_name} not found in region {region}'.format(
+                cluster_name=cluster_name, region=self.region
+            )
+        )
+
+    @staticmethod
+    def construct_step_dict_for_command(step_name, command, action_on_failure='CONTINUE'):
+        return {
+            'Name': step_name,
+            'ActionOnFailure': action_on_failure,
+            'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': command},
+        }
+
     def add_tags(self, context, tags, cluster_id):
         '''Add tags in the dict *tags* to cluster *cluster_id*. Do nothing
-        if *tags* is empty or ``None``'''
+        if *tags* is empty or ``None``.'''
         check.opt_dict_param(tags, 'tags')
         check.str_param(cluster_id, 'cluster_id')
 
--- python_modules/libraries/dagster-aws/dagster_aws_tests/emr_tests/test_emr.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/emr_tests/test_emr.py
@@ -22,3 +22,29 @@ def test_emr_describe_cluster(emr_cluster_config):
     cluster_info = cluster.describe_cluster(cluster_id)
     assert cluster_info['Name'] == 'test-emr'
     assert EmrClusterState(cluster_info['Status']['State']) == EmrClusterState.Waiting
+
+
+@mock_emr
+def test_emr_id_from_name(emr_cluster_config):
+    context = create_test_pipeline_execution_context()
+    cluster = EmrJobRunner(region=REGION)
+    cluster_id = cluster.run_job_flow(context, emr_cluster_config)
+    assert cluster.cluster_id_from_name('test-emr') == cluster_id
+
+
+def test_emr_construct_step_dict():
+    cmd = ['pip', 'install', 'dagster']
+
+    assert EmrJobRunner.construct_step_dict_for_command('test_step', cmd) == {
+        'Name': 'test_step',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': cmd},
+    }
+
+    assert EmrJobRunner.construct_step_dict_for_command(
+        'test_second_step', cmd, action_on_failure='CANCEL_AND_WAIT'
+    ) == {
+        'Name': 'test_second_step',
+        'ActionOnFailure': 'CANCEL_AND_WAIT',
+        'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': cmd},
+    }
