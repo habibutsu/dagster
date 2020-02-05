f90b64036d9d3702532a68c5d54428cd212f7d2a
Nate Kupp <nate@elementl.com>
2020-02-05 10:09:45 -0800

    Pulling in a few EMR updates
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D1955

--- python_modules/libraries/dagster-aws/dagster_aws_tests/emr_tests/test_emr.py
+++ python_modules/libraries/dagster-aws/dagster_aws_tests/emr_tests/test_emr.py
@@ -1,4 +1,5 @@
-from dagster_aws.emr import EmrClusterState, EmrJobRunner
+import pytest
+from dagster_aws.emr import EmrClusterState, EmrError, EmrJobRunner
 from moto import mock_emr
 
 from dagster.utils.test import create_test_pipeline_execution_context
@@ -22,3 +23,37 @@ def test_emr_describe_cluster(emr_cluster_config):
     cluster_info = cluster.describe_cluster(cluster_id)
     assert cluster_info['Name'] == 'test-emr'
     assert EmrClusterState(cluster_info['Status']['State']) == EmrClusterState.Waiting
+
+
+@mock_emr
+def test_emr_id_from_name(emr_cluster_config):
+    context = create_test_pipeline_execution_context()
+    cluster = EmrJobRunner(region=REGION)
+    cluster_id = cluster.run_job_flow(context, emr_cluster_config)
+    assert cluster.cluster_id_from_name('test-emr') == cluster_id
+
+    with pytest.raises(EmrError) as exc_info:
+        cluster.cluster_id_from_name('cluster-doesnt-exist')
+
+    assert (
+        'dagster_aws.emr.emr.EmrError: cluster cluster-doesnt-exist not found in region us-west-1'
+        in str(exc_info)
+    )
+
+
+def test_emr_construct_step_dict():
+    cmd = ['pip', 'install', 'dagster']
+
+    assert EmrJobRunner.construct_step_dict_for_command('test_step', cmd) == {
+        'Name': 'test_step',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': cmd},
+    }
+
+    assert EmrJobRunner.construct_step_dict_for_command(
+        'test_second_step', cmd, action_on_failure='CANCEL_AND_WAIT'
+    ) == {
+        'Name': 'test_second_step',
+        'ActionOnFailure': 'CANCEL_AND_WAIT',
+        'HadoopJarStep': {'Jar': 'command-runner.jar', 'Args': cmd},
+    }
